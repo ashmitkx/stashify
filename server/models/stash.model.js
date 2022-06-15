d776d5c8a67df5783948c2b8cf9e77444cac12fc
ashmitkx <ashmitk0507@gmail.com>
2022-06-16 00:19:32 +0530

    change stash model to have one stash per playlist
    instead of stashes being snapshots,
    they are now a single collection per playlist.

--- server/models/stash.model.js
+++ server/models/stash.model.js
@@ -5,30 +5,31 @@ const stashSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        index: true,
         match: [/^[A-Za-z0-9]{25}$/, 'Invalid spotify user id.'] // spotify uses base-62 ids of length 25
     },
     playlist_id: {
         type: String,
         required: true,
         trim: true,
-        index: true,
+        unique: true,
         match: [/^[A-Za-z0-9]{22}$/, 'Invalid playlist id.'] // spotify uses base-62 ids of length 22
     },
-    track_ids: {
-        type: [
-            {
+    tracks: [
+        {
+            id: {
                 type: String,
+                required: true,
                 trim: true,
                 match: [/^[A-Za-z0-9]{22}$/, 'Invalid track id.'] // spotify uses base-62 ids of length 22
-            }
-        ],
-        validate: [val => val.length > 0, 'Stash must have atleast 1 track_id.']
-    },
-    date: {
-        type: Date,
-        default: () => new Date()
-    }
+            },
+            date_stashed: {
+                type: Date,
+                required: true,
+                default: () => new Date()
+            },
+            _id: false
+        }
+    ]
 });
 
 export const Stash = mongoose.model('stash', stashSchema);
